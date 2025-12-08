f01f33bd0fa2dddf4dc1cc911dc619dfe774ee1b
Andrii Dovbush <xdovbush@fi.muni.cz>
2025-12-08 21:49:20 +0100

    Now /calendar is default

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-	/* config options here */
+	redirects: async () => [
+		{
+			source: '/',
+			destination: '/calendar',
+			permanent: true
+		}
+	]
 };
 
 export default nextConfig;
